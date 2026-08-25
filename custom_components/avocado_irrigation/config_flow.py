62021c5a8facf3a995167ea6c959ac4ccf9014e4
Dreamer41 <106725009+Dreamer41@users.noreply.github.com>
2026-08-25 17:05:22 +0700

    Add Home Assistant entity selectors to setup flow

--- custom_components/avocado_irrigation/config_flow.py
+++ custom_components/avocado_irrigation/config_flow.py
@@ -5,8 +5,17 @@ from __future__ import annotations
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.const import CONF_NAME
+from homeassistant.helpers import selector
 
-from .const import DEFAULT_NAME, DOMAIN
+from .const import (
+    CONF_IRRIGATION_VALVE,
+    CONF_NAME,
+    CONF_OUTDOOR_TEMPERATURE,
+    CONF_PUMP_POWER,
+    CONF_RAIN_GAUGE,
+    DEFAULT_NAME,
+    DOMAIN,
+)
 
 
 class AvocadoIrrigationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -27,6 +36,20 @@ class AvocadoIrrigationConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             data_schema=vol.Schema(
                 {
                     vol.Required(CONF_NAME, default=DEFAULT_NAME): str,
+                    vol.Required(CONF_IRRIGATION_VALVE): selector.EntitySelector(
+                        selector.EntitySelectorConfig(domain="switch")
+                    ),
+                    vol.Required(CONF_RAIN_GAUGE): selector.EntitySelector(
+                        selector.EntitySelectorConfig(
+                            domain=["binary_sensor", "sensor", "counter"]
+                        )
+                    ),
+                    vol.Optional(CONF_PUMP_POWER): selector.EntitySelector(
+                        selector.EntitySelectorConfig(domain="sensor")
+                    ),
+                    vol.Optional(CONF_OUTDOOR_TEMPERATURE): selector.EntitySelector(
+                        selector.EntitySelectorConfig(domain="sensor")
+                    ),
                 }
             ),
         )
